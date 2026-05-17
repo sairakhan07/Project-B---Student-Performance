6a513de99c1c1b6c6c6a9046d34c6c2cbf2ed0a7
sairakhan07 <sairaislamkhan@gmail.com>
2026-05-18 00:29:17 +0100

    Add getters and setters for Module and Student classes

--- Module.java
+++ Module.java
@@ -9,16 +9,57 @@ public class Module //public means that this class can be accessed by any other 
         this.moduleName = moduleName;
     }
 
+    public String getModuleCode() 
+    {
+        return moduleCode; 
+    }
+    
+    public void setModuleCode(String moduleCode) 
+    {
+        this.moduleCode = moduleCode; 
+    }
+    
+    public String getModuleName() 
+    { 
+        return moduleName; 
+    }
+
+    public void setModuleName(String moduleName) 
+    { 
+        this.moduleName = moduleName; 
+    }
+
 }
 
 public class Student 
 {
     private String studentId;
     private String name;
+  
 
     public Student(String studentId, String name) 
     {
         this.studentId = studentId;
         this.name = name;
     }   
+
+    public String getStudentId() 
+    {
+        return studentId; 
+    }
+
+    public void setStudentId(String studentId) 
+    {
+        this.studentId = studentId; 
+    }
+
+    public String getName() 
+    { 
+        return name; 
+    }
+
+    public void setName(String name) 
+    { 
+        this.name = name; 
+    }
 }
